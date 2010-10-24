eda675188b5b769902037dfd83b29b6495270d8d
Leo Molas <leos.molas@gmail.com>
2010-10-24 18:13:39 -0300

    agregados casos de test. agregados partes semanticas

--- casosDeTest.ml
+++ casosDeTest.ml
@@ -1,5 +1,10 @@
+2.0 / 3.0;
+fun f x = (false,2.0*3.6/x);
+fun f x = (x,false,2.0*3.6/x,x);
 fun f x y = (1,y x,x+1);
 fun f x y = (1,x,x+1);
 fun f x y = (1,y x,x+1,y, [y]);
 fun f x y = (1,y x,x+1,y, [x]);
 fun f x y = x [y] (1,y);
+fun g x y = 1;
+f g;
